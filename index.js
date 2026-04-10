e1429d365c1ba97b31366d6837f3bd2394b17d78
Rajeev Rawat <rajeevrawat2100@email.com>
2026-04-10 07:24:30 +0530

    feat: add GET, PUT, DELETE APIs for the lead data

--- index.js
+++ index.js
@@ -1,84 +1,159 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-const Lead = require('./models/lead.model.js')
-const SalesAgent = require('./models/salesAgent.model.js')
-const comment = require('./models/comment.model.js')
-const Tags = require('./models/tag.model.js')
+const Lead = require("./models/lead.model.js");
+const SalesAgent = require("./models/salesAgent.model.js");
+const comment = require("./models/comment.model.js");
+const Tags = require("./models/tag.model.js");
 
+const { initializeDatabase } = require("./db/db.connect.js");
+initializeDatabase();
 
-const { initializeDatabase } = require('./db/db.connect.js')
-initializeDatabase()
-
-async function createSalesAgent(newAgent){
-    try {
-        const agent = new SalesAgent(newAgent)
-        const savedAgent = await agent.save()
-        return savedAgent 
-    } catch (error) {
-        throw error
-    }
+async function createSalesAgent(newAgent) {
+  try {
+    const agent = new SalesAgent(newAgent);
+    const savedAgent = await agent.save();
+    return savedAgent;
+  } catch (error) {
+    throw error;
+  }
 }
 
-app.post('/agents', async (req,res) => {
-    try {
-        const agent = await createSalesAgent(req.body)
-        if(agent){
-            res.status(201).json({message: 'Agent successfully loaded', data: agent})
-        }else{
-            res.status(404).json({error: 'Something went wrong in this agent Data'})
-        }
-    } catch (error) {
-        res.status(500).json({error: 'Failed to fetch Agent Data'})
+app.post("/agents", async (req, res) => {
+  try {
+    const agent = await createSalesAgent(req.body);
+    if (agent) {
+      res
+        .status(201)
+        .json({ message: "Agent successfully loaded", data: agent });
+    } else {
+      res
+        .status(404)
+        .json({ error: "Something went wrong in this agent Data" });
     }
-})
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch Agent Data" });
+  }
+});
 
-async function getAllSalesAgent(){
-    try {
-        const agent = await SalesAgent.find()
-        console.log(agent)
-        return agent
-    } catch (error) {
-        throw error
-    }
+async function getAllSalesAgent() {
+  try {
+    const agent = await SalesAgent.find();
+    return agent;
+  } catch (error) {
+    throw error;
+  }
 }
 
-app.get('/agents', async (req,res) => {
-    try {
-        const agent = await getAllSalesAgent()
-        res.status(201).json({message: 'All Sales Data is this', data: agent})
-    } catch (error) {
-        res.status(500).json({error: 'Failed to fetch Sales Data'})
-    }
-})
+app.get("/agents", async (req, res) => {
+  try {
+    const agent = await getAllSalesAgent();
+    res.status(201).json({ message: "All Sales Data is this", data: agent });
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch Sales Data" });
+  }
+});
 
-async function createALead(newLead){
-    try {
-        const lead = new Lead(newLead)
-        const savedLead = await lead.save()
-        return savedLead
-    } catch (error) {
-        throw error
-    }
+async function createALead(newLead) {
+  try {
+    const lead = new Lead(newLead);
+    const savedLead = await lead.save();
+    return savedLead;
+  } catch (error) {
+    throw error;
+  }
 }
 
-app.post('/leads', async (req, res) => {
-    try {
-        const lead = await createALead(req.body)
-        if(lead){
-            res.status(201).json({message: 'Lead successfully loaded', data: lead})
-        }else{
-            res.status(404).json({error: 'Something went wrong in this lead'})
-        }
-    } catch (error) {
-        res.status(500).json({error: 'Failed to fetch lead Data'})
-        console.error(error.message)
+app.post("/leads", async (req, res) => {
+  try {
+    const lead = await createALead(req.body);
+    if (lead) {
+      res.status(201).json({ message: "Lead successfully loaded", data: lead });
+    } else {
+      res.status(404).json({ error: "Something went wrong in this lead" });
     }
-}) 
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch lead Data" });
+    // console.error(error.message);
+  }
+});
 
-const PORT = 3001
+async function getAllLeadsData() {
+  try {
+    const leads = await Lead.find().populate("salesAgent");
+    return leads;
+  } catch (error) {
+    throw error;
+  }
+}
+
+app.get("/leads", async (req, res) => {
+  try {
+    const { status, salesAgent, source } = req.query;
+    // console.log(status, salesAgent, source);
+    const leads = await getAllLeadsData();
+    if (leads) {
+      res.status(200).json({ message: "All Lead Data this", data: leads });
+    } else {
+      res.status(400).json({ error: "Something wrong is the Data" });
+    }
+  } catch (error) {
+    throw error;
+  }
+});
+
+async function updatedLeadByLeadId(leadId, dataToUpdate) {
+  try {
+    const lead = await Lead.findByIdAndUpdate(leadId, dataToUpdate, {
+      new: true,
+    });
+    return lead;
+  } catch (error) {
+    throw error;
+  }
+}
+
+app.put("/leads/:leadId", async (req, res) => {
+  try {
+    const lead = await updatedLeadByLeadId(req.params.leadId, req.body);
+    if (lead) {
+      res
+        .status(201)
+        .json({ message: "Lead Data Updated Successfully", data: lead });
+    } else {
+      res.status(404).json({ error: "Lead Id not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch lead data" });
+  }
+});
+
+async function deletedLeadByLeadId(leadId) {
+  try {
+    const lead = await Lead.findByIdAndDelete(leadId);
+    return lead;
+  } catch (error) {
+    throw error;
+  }
+}
+
+app.delete("/leads/:leadId", async (req, res) => {
+  try {
+    const lead = await deletedLeadByLeadId(req.params.leadId);
+    if (lead.length > 0) {
+      res.status(201).json({ message: "Lead deleted successfully" });
+    } else {
+      res.status(404).json({ error: "Lead with Id not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch lead Details" });
+  }
+});
+
+const PORT = 3001;
 app.listen(PORT, () => {
-    console.log('Server is running on 3001')
-})
+  console.log("Server is running on 3001");
+});
