7935f7e948af4d6b8c82eb18651c4d5feb773eed
Rajeev Rawat <rajeevrawat2100@email.com>
2026-04-16 13:43:41 +0530

    chore: cors setup is done

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
+const cors = require('cors')
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(cors())
+
 
 const Lead = require("./models/lead.model.js");
 const SalesAgent = require("./models/salesAgent.model.js");
